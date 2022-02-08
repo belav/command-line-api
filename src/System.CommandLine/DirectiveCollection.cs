b410ba5329d1358fc1b37727897e178fd8773c4d
Bela VanderVoort <bela.vandervoort@optimizely.com>
2022-02-08 16:04:15 -0600

    0.15.0

--- src/System.CommandLine/DirectiveCollection.cs
+++ src/System.CommandLine/DirectiveCollection.cs
@@ -10,10 +10,8 @@ namespace System.CommandLine
 {
     internal class DirectiveCollection : IDirectiveCollection
     {
-        private readonly Dictionary<string, List<string>> _directives = new Dictionary<
-            string,
-            List<string>
-        >();
+        private readonly Dictionary<string, List<string>> _directives =
+            new Dictionary<string, List<string>>();
 
         public void Add(string name, string? value)
         {
